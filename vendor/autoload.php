66224d53de580d8811029a984c5a4d7b6f90eb76
KC Ng <nkc900201@gmail.com>
2021-07-31 22:04:49 +0900

    Install laravel in template framework

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit93532f1ef4653ec45500de0e97c09fbd::getLoader();
+return ComposerAutoloaderInit929983ffadbb4835f5653c9c462ece1f::getLoader();
